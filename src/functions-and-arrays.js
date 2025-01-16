b4aa989dc59a97b7aba006d164e32794b0949d9d
hamid89 <hamid.khurshid24@gmail.com>
2025-01-16 15:11:53 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1,num2) {
+    if(num1 > num2){
+        return num1
+    }else if (num1 < num2){
+        return num2
+    } else {
+        return num1,num2
+    }
+}
+
+console.log(maxOfTwoNumbers(2,3))
 
 
 
@@ -7,7 +17,21 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(Arr) {
+    if (Arr.length === 0){
+        return null
+    }else{
+    let word = Arr[0]
+    for (const val of Arr){
+        if (val.length > word.length){
+            word = val
+        }
+    }
+    return word
+}
+}
+
+console.log(findLongestWord(words))
 
 
 
@@ -15,15 +39,32 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(Arr) {
+    let sum = 0
+    for (const val of Arr){
+        sum +=val
+    }
+    return sum
+}
 
+console.log(sumNumbers(numbers))
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(Arr) {
+    if (Arr.length === 0){
+        return 0
+    }
+    let sum = 0
+    for (const val of Arr){
+        sum +=val
+    }
+    return sum/Arr.length
+}
+console.log(averageNumbers(numbers2))
 
 
 
@@ -31,4 +72,20 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(Arr,tWord){
+   if (Arr.length === 0){
+    return null
+   }
+    for (const val of Arr){
+        if (val === tWord){
+            return true
+        } 
+        }
+    
+        return false
+    
+    
+   }
+  
+   console.log(doesWordExist(numbers,'hamid'))
+
